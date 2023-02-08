d21bdf125afdd566d0b2df90d98f0547948cb161
Deontae Carter <deontaecarter93@gmail.com>
2023-02-08 02:10:43 +0000

    this is my message

---
+++ helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge My first script
+# Author: Deontae Carter
+# Date of latest revision: 2/7/2023
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo "I am working perfectly"
+
+# End
